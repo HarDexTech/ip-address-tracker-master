a028815274d8e01f1a2347582d99f0b42aa96cbd
ADESINA AYOMIDE <adesinayomide2@gmail.com>
2025-12-22 14:24:17 +0100

    Add Leaflet integration for map display and enhance search functionality

--- script.js
+++ script.js
@@ -5,8 +5,14 @@ const API_KEY = 'at_80uQgCkKAotkZOGru85gCDbqaffrU';
 const searchInput = document.getElementById('searchInput');
 const searchBtn = document.querySelector('.search-button');
 const notification = document.querySelector('.notification');
-const ipAddress = document.getElementById('ipAddress');
+const ipAddressContent = document.getElementById('ipAddress');
+const locationContent = document.getElementById('location');
+const timeZoneContent = document.getElementById('timezone');
+const ispContent = document.getElementById('isp');
 let result;
+let latitude;
+let longitude;
+let map = null;
 
 function notificationFunc(msg) {
   notification.textContent = msg;
@@ -24,14 +30,45 @@ function searchBtnFunc() {
   fetch(
     `https://geo.ipify.org/api/v2/country,city?apiKey=${API_KEY}&ipAddress=${searchInput.value}`
   )
-    .then((response) => {
-      if (!response.ok) {
-        notification('Error fetching data');
-      }
-      return response.json();
-    })
+    .then((response) => response.json())
     .then((data) => {
-      console.log(data);
+      const ipAddress = data.ip;
+      const isp = data.isp;
+      const timeZone = data.location.timezone;
+      const city = data.location.city;
+      const country = data.location.country;
+      latitude = data.location.lat;
+      longitude = data.location.lng;
+
+      ipAddressContent.textContent = ipAddress;
+      locationContent.textContent = `${city}, ${country}`;
+      timeZoneContent.textContent = timeZone;
+      ispContent.textContent = isp;
+
+      createOrUpdateMap();
+    })
+    .catch((error) => {
+      notificationFunc('Error');
+      console.error('Error:', error);
     });
 }
 searchBtn.addEventListener('click', searchBtnFunc);
+
+function createOrUpdateMap() {
+  // Remove existing map if one is already displayed
+  if (map) {
+    map.remove();
+  }
+  // Create new map centered on the coordinates with zoom level 13
+  map = L.map('map').setView([latitude, longitude], 13);
+
+  // Add OpenStreetMap tile layer to the map
+  L.tileLayer('https://{s}tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution:
+      '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+  }).addTo(map);
+
+  // Add a marker pin at the searched coordinates
+  var marker = L.marker([latitude, longitude]).addTo(map);
+}
